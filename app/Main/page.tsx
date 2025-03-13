daf1eff805076ec9cc7cf72d731fcb100e362a3b
M-Jey <mjeyict3@gmail.com>
2025-03-13 16:57:44 +0800

    Add navigation bar to Main page and include react-icons dependency

--- app/Main/page.tsx
+++ app/Main/page.tsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import { GoPlusCircle } from "react-icons/go";
+
 export default function Main() {
   return (
     <>
       <div className="bg-white w-full h-[100vh] text-black">
-        <div className="">Main Page</div>
+        <div className="w-full h-[70px] bg-[#b9b9b9] flex justify-center items-center">
+          <ul className="flex justify-between w-full p-5">
+            <li>
+              <Link legacyBehavior href="/">
+                <div className="font-pencerio text-xl font-bold cursor-pointer" title="go back to landing">Lace Link</div>
+              </Link>
+            </li>
+            <li className="flex items-center cursor-pointer">
+              <div className="p-1">Create a Post</div>
+              <GoPlusCircle size={25} />
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
